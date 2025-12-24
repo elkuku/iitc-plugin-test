9d7aeffa18c17c9a894f89a1f318bf7673233053
elkuku <der.el.kuku@gmail.com>
2025-12-24 07:26:37 -0500

    Update

--- bin/setup-github-page.js
+++ bin/setup-github-page.js
@@ -1,1 +1,36 @@
-console.log("Hello, World!")
+import fs from 'fs'
+
+console.log('Generating GitHub page...')
+
+fs.rmSync('gh_page', { recursive: true, force: true });
+fs.mkdirSync('gh_page', { recursive: true });
+
+fs.cpSync('dist', 'gh_page/files', { recursive: true });
+fs.cpSync('.github/page', 'gh_page', { recursive: true });
+
+const fileNames = fs
+    .readdirSync('dist', { withFileTypes: true })
+    .filter(entry => entry.isFile())
+    .map(entry => entry.name)
+    .filter(name => !name.endsWith('meta.js'))
+
+const pluginData = JSON.parse(
+    fs.readFileSync('plugin.json', 'utf8')
+)
+
+const baseUrl = '/files'
+const links = fileNames
+    .map(name => `<li><a href="${baseUrl}/${name}">${name}</a></li>`)
+    .join("\n");
+
+let template = fs.readFileSync('gh_page/index.html', 'utf8');
+
+const projectName = pluginData.name.replace('IITC plugin: ', '')
+
+template = template.replace("{{FILE_LINKS}}", links);
+template = template.replaceAll("{{PROJECT_NAME}}", projectName);
+template = template.replace("{{PROJECT_DESCRIPTION}}", pluginData.description);
+
+fs.writeFileSync('gh_page/index.html', template, 'utf8');
+
+console.log('Finished =;)')
