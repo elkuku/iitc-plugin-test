27747b18ce3b0ce383698fd46d2db61e3da1959c
elkuku <der.el.kuku@gmail.com>
2025-12-24 18:27:27 -0500

    Add more stuff

--- src/Main.ts
+++ src/Main.ts
@@ -7,16 +7,12 @@ class IitcPluginTest implements Plugin.Class {
         console.log("IitcPluginTest " + VERSION);
 
         // eslint-disable-next-line unicorn/prefer-module, @typescript-eslint/no-require-imports
-        require("./styles.css");
+       // require("./styles.css");
 
         // FILL ME
+        alert('Hello IitcPluginTest');
     }
 
 }
 
-/**
- * use "main" to access you main class from everywhere
- * (same as window.plugin.IitcPluginTest)
- */
-export const main = new IitcPluginTest();
-Plugin.Register(main, "IitcPluginTest");
+Plugin.Register(new IitcPluginTest(), "IitcPluginTest");
